8d2f4fc22c6946a7dc1cffaf5bf91087364f1ca0
Ryuukae <ryuukae.dev@gmail.com>
2026-08-03 18:39:24 -0400

    feat(core): implement Fisher-Yates array shuffle utility
    - Added `shuffleQuizData` method to the QuizState class.
    - Utilized the Fisher-Yates algorithm for unbiased, in-place array randomization.
    - Established a pure utility function to support upcoming question and answer deck randomization.

--- QuizState.js
+++ QuizState.js
@@ -74,4 +74,17 @@ class QuizState {
     isQuizOver() {
         return this.index >= this.questionData.length;
     }
+
+    /**
+     * Shuffles an array in-place using the Fisher-Yates algorithm.
+     * @param {Array} array - The array to shuffle.
+     * @returns {Array} The shuffled array.
+     */
+    shuffleQuizData(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
 }
