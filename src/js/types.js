e092879a990fd0724674f7a0677d99f86d4fda0e
Ryuukae <ryuukae.dev@gmail.com>
2026-08-20 00:53:29 -0400

    chore(types): define editor screen and modal callback typedefs
    - Added JSDoc typedef definitions for editor screen states and modal callbacks.
    - Documented component interface contracts and data payloads.

--- src/js/types.js
+++ src/js/types.js
@@ -20,7 +20,27 @@
  */
 
 /**
- * @typedef {"start" | "creator" | "quiz" | "result"} ScreenKey
+ * Type definition for a distractor option object.
+ * @typedef {object} DistractorObject
+ * @property {string} text - The distractor text.
+ */
+
+/**
+ * Type definition for raw question payload structure (e.g. QAD format prefill data).
+ * @typedef {object} RawQuestionType
+ * @property {string} question - The question text.
+ * @property {string} [correct_answer] - The correct answer string.
+ * @property {Array<DistractorObject | string>} [distractors] - The array of distractor options.
+ * @property {AnswerType[]} [answers] - Alternate array of choices.
+ */
+
+/**
+ * Union type for question data supplied to builder card components and export utilities.
+ * @typedef {QuestionType | RawQuestionType} BuilderCardPrefillType
+ */
+
+/**
+ * @typedef {"start" | "creator" | "editor" | "quiz" | "result"} ScreenKey
  */
 
 /**
@@ -28,11 +48,42 @@
  */
 
 /**
+ * Interface definition for a scoped logger instance.
+ * @typedef {object} LoggerInstance
+ * @property {(message: string, details?: any) => void} trace - Emits a trace log.
+ * @property {(message: string, details?: any) => void} debug - Emits a debug log.
+ * @property {(message: string, details?: any) => void} info - Emits an info log.
+ * @property {(message: string, details?: any) => void} warn - Emits a warn log.
+ * @property {(message: string, details?: any) => void} error - Emits an error log.
+ * @property {(childScope: string) => LoggerInstance} child - Spawns a child logger.
+ */
+
+/**
+ * Union of all modal element IDs present in the application.
+ * @typedef {"creation-mode-modal" | "modal-guide-txt" | "modal-guide-json" | "modal-focus-edit" | "modal-focus-editor"} ModalId
+ */
+
+/**
+ * Staged file upload data structure for modification.
+ * @typedef {object} ModifyQuizPayloadType
+ * @property {BuilderCardPrefillType[]} parsedQuestions - Array of parsed question objects ready for builder ingestion.
+ * @property {string} rawText - The unparsed source text content for the text editor.
+ * @property {string} fileName - The name of the ingested source file.
+ */
+
+/**
  * @typedef {import('./models/QuizState.js').default} QuizStateType
  * @typedef {import('./models/BuilderState.js').default} BuilderStateType
  * @typedef {import('./controllers/AppNavigationController.js').default} AppNavigationControllerType
+ * @typedef {import('./controllers/StartUIController.js').default} StartUIControllerType
  * @typedef {import('./controllers/QuizUIController.js').default} QuizUIControllerType
+ * @typedef {import('./controllers/BuilderUIController.js').default} BuilderUIControllerType
+ * @typedef {import('./controllers/EditorUIController.js').default} EditorUIControllerType
  * @typedef {import('./components/BuilderCardComponent.js').default} BuilderCardComponentType
+ * @typedef {import('./components/QuestionFocusModalComponent.js').default} QuestionFocusModalComponentType
+ * @typedef {import('./components/FullscreenEditorModalComponent.js').default} FullscreenEditorModalComponentType
+ * @typedef {import('./utils/logger.js').default} LoggerType
+ * @typedef {import('./utils/StorageService.js').default} StorageServiceType
  */
 
 export {};
