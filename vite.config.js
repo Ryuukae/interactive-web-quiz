17eb34c70eb3b1fa79c76ac031adf41d1c713aa9
Ryuukae <ryuukae.dev@gmail.com>
2026-08-20 19:02:16 -0400

    fix(config): set vite base path to resolve 404 assets on github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/interactive-web-quiz/",
   server: {
     port: 5173,
     strictPort: true
